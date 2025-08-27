47ee578fb3f6b0a2f0cfb8bb020c65f7eab6703b
Tin210303 <tinnguyen210303@gmail.com>
2025-08-27 11:21:49 +0700

    i u ì à

--- src/pages/BlogDetail/RecentPostsSection/index.tsx
+++ src/pages/BlogDetail/RecentPostsSection/index.tsx
@@ -1,6 +1,6 @@
 import { Eye, Heart } from "lucide-react";
 import { useState } from "react";
-import styles from '.index.module.css';
+import styles from '../index.module.css';
 
 interface RecentPost {
     id: number;
